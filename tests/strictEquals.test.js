af38da11f1b3373580a8641de704180ba999910b
benjamingray20 <60855283+benjamingray20@users.noreply.github.com>
2020-03-09 10:50:52 -0400

    created the all my tests in the strictEquals.test.js file

---
+++ tests/strictEquals.test.js
@@ -0,0 +1,56 @@
+const describe = require('mocha').describe
+const it = require('mocha').it
+const expect = require('chai').expect
+const strictEquals = require('../strictEquals')
+
+describe('Equals', () => {
+  describe('strictEquals', () => {
+    it('returns true if values are same type and same value', () => {
+      //create test data
+      const checkNumberOne = 'test'
+      const checkNumberTwo = 'test'
+
+      //call the function with the test data
+      const checkResult = strictEquals(checkNumberOne, checkNumberTwo)
+
+      //make assertions about what should be true
+      expect(checkResult).to.equal(true)
+    })
+
+    it('returns false when values are same type but different values', () => {
+      //create test data
+      const checkNumberOne = 5
+      const checkNumberTwo = 10
+
+      //call the function with the test data
+      const checkResult = strictEquals(checkNumberOne, checkNumberTwo)
+
+      //make assertions about what should be true
+      expect(checkResult).to.equal(false)
+    })
+
+    it('returns false when values are different types but same value', () => {
+      //create test data
+      const checkNumberOne = 5
+      const checkNumberTwo = '5'
+
+      //call the function with the test data
+      const checkResult = strictEquals(checkNumberOne, checkNumberTwo)
+
+      //make assertions about what should be true
+      expect(checkResult).to.equal(false)
+    })
+
+    it('returns false when values are different types', () => {
+      //create test data
+      const checkNumberOne = 5
+      const checkNumberTwo = '10'
+
+      //call the function with the test data
+      const checkResult = strictEquals(checkNumberOne, checkNumberTwo)
+
+      //make assertions about what should be true
+      expect(checkResult).to.equal(false)
+    })
+  })
+})
